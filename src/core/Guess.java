525670b9e0c5e7b153996139648fb74d22db6537
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 15:36:56 +0300

    Class to store a PieceSet and a Comparison

---
+++ src/core/Guess.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Guess {
+    private final PieceSet guess;
+    private final Comparison correctness;
+
+    public Guess(PieceSet guess, PieceSet goal) {
+        this.guess = guess;
+        correctness = guess.compare(goal);
+    }
+
+    public PieceSet getGuess() {
+        return guess;
+    }
+
+    public Comparison getCorrectness() {
+        return correctness;
+    }
+}
